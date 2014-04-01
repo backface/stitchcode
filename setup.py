fb5f09b8025b805ed2202fc4426831abadb83df2
Michael Aschauer <m@ash.to>
2014-04-01 22:02:13 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(	name="stitchcode",
+		version="0.01",
+		author="Michael Aschauer",
+		py_modules=['stitchcode'],
+		data_files=[	('bin', ['exp2png.py','exp2exp.py'])]
+     )
