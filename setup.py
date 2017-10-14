3d95e339367892714244f6c66438fe1ac0c09289
Michael Aschauer <m@ash.to>
2017-10-14 14:15:11 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
 
+
 setup(	name="stitchcode",
-		version="0.01",
+		version="0.2",
 		author="Michael Aschauer",
+		author_email="m@ash.to",
 		py_modules=['stitchcode'],
-		data_files=[
-		('bin', 
-			['exp2png.py',
-			'exp2exp.py',
-			'exp2svg.py',
-			'stitchconv.py']
-		)]
+		license='LICENSE.txt',
+		description='Embroidery tools for python',
+		long_description=open('README.txt').read(),
+		url='https://github.com/backface/stitchcode',
+		#data_files=[
+		#('bin', 
+		#	['exp2png.py',
+		#	'exp2exp.py',
+		#	'exp2svg.py',
+		#	'stitchconv.py']
+		#)]
+		scripts=['stitchconv.py'],
      )
